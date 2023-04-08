38a316d0d9785b0154228e3c59135e7a74f73e3f
Nalinaka <nalinaka@gmail.com>
2023-04-08 09:59:22 -0700

    Completed

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-function newImage (url,left,bottom) {
-let object = document.createElement('img')
-object.src = 'assets/green-character.gif'
-object.style.position = 'fixed'
-object.style.left = '100px'
-object.style.bottom = '100px'
-document.body.append(object)
-return object
+function newImage(url, left, bottom){
+    let object = document.createElement('img')
+    object.src = url
+    object.style.position = 'fixed'
+    object.style.left = left + 'px'
+    object.style.bottom = bottom + 'px'
+    document.body.append(object)
+    return object
 }
 
-newImage() // Logs new image 
-newImage() // Logs new image again 
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
 
-function newImage(url,left,bottom) {
-let object = newImage(url,left,bottom)
-object.addEventListener('dblclick', () => {
-    object.remove()
-})
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
 }
 
 newImage('assets/green-character.gif', 100, 100)
@@ -28,31 +26,3 @@ newImage('assets/well.png', 500, 425)
 newItem('assets/sword.png', 500, 405)
 newItem('assets/shield.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
-
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
-
-let pinetree = document.createElement('img')
-pinetree.src = 'assets/pine-tree.png'
-pinetree.style.position = 'fixed'
-pinetree.style.left = '450px'
-pinetree.style.bottom = '200px'
-document.body.append(pinetree)
-
-let sword = document.createElement('img')
-sword.src = 'assets/sword.png'
-sword.position = 'fixed'
-sword.left = '500px'
-sword.bottom = '405px'
-document.body.append(sword)
-
-sword.addEventListener('click', function(){
-    sword.remove()
-})
-
-let newImage
-document.createElement.
