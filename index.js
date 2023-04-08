87710fe3cce8533a4993fd7a2a64da70be16b4d0
Nalinaka <nalinaka@gmail.com>
2023-04-08 10:20:56 -0700

    With bonus

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function title{url,left,bottom,width,height}{
+    for{let h= 0; h< height; h++}{
+        for{let w = 0; w< width; w++}{
+            newImage{url,left + w+100, bottom+h+100}
+            }
+        }
+    }
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -16,6 +24,14 @@ function newItem(url, left, bottom){
     })
 }
 
+let horizon = window.innerHeight / 1.75
+let heightOfSky = window.innerHeight-horizon
+let heightOfGrass = horizon;
+
+tile('assets/sky.png,' 0, horizon, window.innerWidth / 100, heightOfSky/100);
+title('assets/grass.png', 0,0, window.innerWidth / 100, heightOfGrass/100);
+
+
 newImage('assets/green-character.gif', 100, 100)
 newImage('assets/tree.png', 200, 300)
 newImage('assets/pillar.png', 350, 100)
@@ -24,5 +40,7 @@ newImage('assets/crate.png', 150, 200)
 newImage('assets/well.png', 500, 425)
 
 newItem('assets/sword.png', 500, 405)
-newItem('assets/shield.png', 165, 185)
+newItem('assets/sheild.png', 165, 185)
 newItem('assets/staff.png', 600, 100)
+
+
