11f1b56f5b47cafe1a316ad1696e3429bc0ee3e8
Nalinaka <nalinaka@gmail.com>
2023-04-12 13:19:27 -0700

    Finally completed

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
-function tile{url,left,bottom,width,height} {
-    for  {let h= 0; h< height; h++}{
-        for(let w = 0; w< width; w++){
-            newImage{url,left + w+100, bottom+h+100};
-    }
-    }
+
+function newImage(url,left,bottom){
+let newImage = document.createElement('img')
+newImage.src = url
+newImage.style.position = 'fixed'
+newImage.style.left = left + 'px'
+newImage.style.bottom = bottom + 'px'
+document.body.append(newImage)
 }
 
-function newImage(url, left, bottom){
-    let object = document.createElement('img')
-    object.src = url
-    object.style.position = 'fixed'
-    object.style.left = left + 'px'
-    object.style.bottom = bottom + 'px'
-    document.body.append(object)
-    return object
+function newItem(url,left,bottom){
+    let newItem = document.createElement('img')
+    newItem.src = url
+    newItem.style.position = 'fixed'
+    newItem.style.left = left + 'px'
+    newItem.style.bottom = bottom + 'px'
+    document.body.append(newItem)
+
+newItem.addEventListener('dblclick', function(){
+    newItem.remove()
+})
+
 }
 
-function newItem(url, left, bottom){
-    let object = newImage(url, left, bottom)
-
-    object.addEventListener('dblclick', () => {
-        object.remove()
-    });
-}
-
-let horizon = window.innerHeight / 1.75
-let heightOfSky = window.innerHeight-horizon
-let heightOfGrass = horizon;
-
-tile('assets/sky.png,' 0, horizon, window.innerWidth / 100, heightOfSky/100);
-title('assets/grass.png', 0,0, window.innerWidth / 100, heightOfGrass/100);
-
-
-newImage('assets/green-character.gif', 100, 100)
-newImage('assets/tree.png', 200, 300)
-newImage('assets/pillar.png', 350, 100)
-newImage('assets/pine-tree.png', 450, 200)
-newImage('assets/crate.png', 150, 200)
-newImage('assets/well.png', 500, 425)
-
-newItem('assets/sword.png', 500, 405)
-newItem('assets/sheild.png', 165, 185)
-newItem('assets/staff.png', 600, 100)
-
-
+    newImage('assets/green-character.gif', 100, 100)
+    newImage('assets/tree.png', 200, 300)
+    newImage('assets/pillar.png', 350, 100)
+    newImage('assets/pine-tree.png', 450, 200)
+    newImage('assets/crate.png', 150, 200)
+    newImage('assets/well.png', 500, 425)
+    newItem('assets/sword.png', 500, 405)
+    newItem('assets/sheild.png', 165, 185)
+    newItem('assets/staff.png', 600, 100)
+    
